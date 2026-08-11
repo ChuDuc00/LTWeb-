d7d52ac066462c0168e19f74c69b2ae60deb2657
Chu Xuan Minh Duc <thuduc19102006@gmail.>
2026-08-11 11:46:00 +0700

    B1: Tao trang PHP dau tien

---
+++ B1/about.php
@@ -0,0 +1,23 @@
+<?php
+  $hoTen = "Chu Xuân Minh Đức"; 
+  $maSV = "224001782";     
+  $lop = "CNTT D2024B";      
+?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>about.php</title>
+</head>
+<body>
+    <h1>Thông Tin Cá Nhân</h1>
+    <p><strong>Họ và tên:</strong> <?php echo $hoTen; ?></p>
+    <p><strong>Mã sinh viên:</strong> <?php echo $maSV; ?></p>
+    <p><strong>Lớp:</strong> <?php echo $lop; ?></p>
+
+    <h2>Các Dự Án Đã Làm</h2>
+    <ul>
+        <li><strong>Dự án Lập trình Web:</strong> bài 1 đến bài 9 LTWeb.</li>
+    </ul>
+</body>
+</html>
